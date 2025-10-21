9562fbc35354707fac8ff0ebc9b42b621e9f246b
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-21 21:21:09 +0700

    Update server.js

--- 20230140193-server/server.js
+++ 20230140193-server/server.js
@@ -1,33 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
+const morgan = require("morgan");
 
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-
-
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
-
-
-const bookRoutes = require('./routes/books');
-app.use('/api/books', bookRoutes);
-
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Resource not found' });
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
 });
-
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-  res.status(500).json({ message: 'Internal Server Error' });
-});
-
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
+
