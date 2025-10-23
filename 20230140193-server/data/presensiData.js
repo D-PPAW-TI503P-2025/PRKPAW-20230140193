93d45c943900e82aa311152c8d9bd6036d7af2d1
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-23 16:03:57 +0700

    update tugas 4

--- 20230140193-server/data/presensiData.js
+++ 20230140193-server/data/presensiData.js
@@ -1,7 +1,7 @@
 const presensiRecords = [
     // Kita bisa mulai dengan data contoh
     {
-      userId: 456,
+      userId: 123,
       nama: 'User Karyawan',
       checkIn: new Date('2025-10-14T08:05:00'),
       checkOut: new Date('2025-10-14T17:00:00')
