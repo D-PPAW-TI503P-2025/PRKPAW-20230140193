bd8a3f736571aa3e7876404e46d883d0d5e6841c
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-13 22:07:44 +0700

    update tugas 7

--- 20230140193-react/src/App.js
+++ 20230140193-react/src/App.js
@@ -1,27 +1,47 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import DashboardPage from './components/DashboardPage';
+
+// 🔒 Komponen proteksi halaman private
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <Router>
       <div>
-        {/* Navigasi ini bisa dihapus jika tidak diperlukan */}
-        <nav className="p-4 bg-gray-100">
-          <Link to="/login" className="mr-4">Login</Link>
-          <Link to="/register">Register</Link>
+        {/* Navigasi opsional */}
+        <nav className="p-4 bg-gray-100 flex justify-center space-x-4">
+          <Link to="/login" className="text-blue-600 hover:underline">Login</Link>
+          <Link to="/register" className="text-blue-600 hover:underline">Register</Link>
         </nav>
-        
+
         <Routes>
+          {/* Halaman login & register */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/" element={<LoginPage />} /> 
+
+          {/* Halaman dashboard yang dilindungi */}
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <DashboardPage />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Redirect root ke login */}
+          <Route path="/" element={<Navigate to="/login" />} />
         </Routes>
       </div>
     </Router>
   );
 }
+
 export default App;
+
